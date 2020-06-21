45134a1e93b013cd5e2a7dce3294280d96258a31
YerToffeeRoll <sam_killen_@hotmail.com>
2020-06-22 00:39:38 +0100

    setup encore webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/assets/')
+    .setPublicPath('/assets')
+    .addEntry('app', './assets/js/app.js')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    .enableSassLoader()
+    .enableVueLoader()
+//.autoProvidejQuery()
+;
+
+module.exports = Encore.getWebpackConfig();
